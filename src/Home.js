7fb065c8e83f5268bbe006565ff1b60a9704a9f5
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-06-10 17:15:08 +0200

    #11: props

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import BlogList from './BlogList'
 
 const Home = () => {
     // let name = 'mario' 
@@ -20,6 +21,7 @@ const Home = () => {
     //     console.log('hello ' + name, e.target)
     // }
 
+    // props - const from parent useable in child
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
@@ -28,14 +30,7 @@ const Home = () => {
 
     return ( 
         <div className="home"> 
-            {/* map-method 
-            key-property - unique(id)*/}
-            {blogs.map(blog => (
-                <div className="blog-preview" key={blog.id} >
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs!" />
 
             {/* <p>{ name } is { age} years old</p>
             <button onClick={ handleClick2 }>Click me</button>
