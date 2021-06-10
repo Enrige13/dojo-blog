15c2deeda9cd42bbbe0aff195bfdf1a8487c547b
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-06-10 15:46:53 +0200

    #4: Dynamic Values in Templates

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import './App.css';
 // import React from 'react' - in older versions 16-
 
 function App() { // jsx - html style templates (bubble rendert html)
+  const title = 'Welcome to the new blog'
+  const likes = 50 
+  // number, strings and arrays are fine
+  // can't output booleans or objects
+  const link = "http://www.google.com"
+  
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+
+        <p>{ 10 }</p>
+        <p>{ "hello, ninjas" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={ link }>Google site</a>
       </div>
     </div>
   );
