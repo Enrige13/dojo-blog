5cf93d93f8c608f681950f4eb8c7f8b7c9d0e19b
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-06-10 17:04:32 +0200

    #10: lists

--- src/Home.js
+++ src/Home.js
@@ -3,27 +3,41 @@ import { useState } from 'react'
 const Home = () => {
     // let name = 'mario' 
     // doesn't change the value - only in console - need hook
-    const [ name, setName ] = useState('mario') // value1 - initial value, value2 (set) - for function
-    const [age, setAge] = useState(25)
+    // const [ name, setName ] = useState('mario') // value1 - initial value, value2 (set) - for function
+    // const [age, setAge] = useState(25)
     
-    const handleClick2 = () => {
-        setName('luigi')
-        setAge(30)
-    } 
+    // const handleClick2 = () => {
+    //     setName('luigi')
+    //     setAge(30)
+    // } 
 
 
-    const handleClick = (e) => {
-        console.log('hello, ninjas', e)
-    }
+    // const handleClick = (e) => {
+    //     console.log('hello, ninjas', e)
+    // }
 
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target)
-    }
+    // const handleClickAgain = (name, e) => {
+    //     console.log('hello ' + name, e.target)
+    // }
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
     return ( 
-        <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age} years old</p>
+        <div className="home"> 
+            {/* map-method 
+            key-property - unique(id)*/}
+            {blogs.map(blog => (
+                <div className="blog-preview" key={blog.id} >
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
+
+            {/* <p>{ name } is { age} years old</p>
             <button onClick={ handleClick2 }>Click me</button>
 
             <br></br>
@@ -31,7 +45,7 @@ const Home = () => {
             <br></br>
 
             <button onClick={ handleClick }>Click me</button>
-            <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
+            <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button> */}
         </div>
     );
 }
