11b62ed2bf11340916b0b2e8ab6ed336549a4d0d
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-06-10 16:55:25 +0200

    #8: Using State (useState hook)

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,16 @@
+import { useState } from 'react'
+
 const Home = () => {
+    // let name = 'mario' 
+    // doesn't change the value - only in console - need hook
+    const [ name, setName ] = useState('mario') // value1 - initial value, value2 (set) - for function
+    const [age, setAge] = useState(25)
+    
+    const handleClick2 = () => {
+        setName('luigi')
+        setAge(30)
+    } 
+
 
     const handleClick = (e) => {
         console.log('hello, ninjas', e)
@@ -11,6 +23,13 @@ const Home = () => {
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age} years old</p>
+            <button onClick={ handleClick2 }>Click me</button>
+
+            <br></br>
+            <br></br>
+            <br></br>
+
             <button onClick={ handleClick }>Click me</button>
             <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
         </div>
