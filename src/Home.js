93e8a67328cf9fb84834aa73c5871f95ddecbec5
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-06-11 13:42:19 +0200

    #20: Making a Custom Hook

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
+import useFetch from './useFetch';
 
 const Home = () => {
     // let name = 'mario' 
@@ -7,54 +8,8 @@ const Home = () => {
     // const [ name, setName ] = useState('mario') // value1 - initial value, value2 (set) - for function
     // const [age, setAge] = useState(25)
     
-    // const handleClick2 = () => {
-    //     setName('luigi')
-    //     setAge(30)
-    // } 
-
-
-    // const handleClick = (e) => {
-    //     console.log('hello, ninjas', e)
-    // }
-
-    // const handleClickAgain = (name, e) => {
-    //     console.log('hello ' + name, e.target)
-    // }
-
     // props - const from parent useable in child
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true) // loading the Data
-    const [error, setError] = useState(null)
-
-    // const [name, setName] = useState('mario')
-
-    // const handleDelete = (id) => {
-    //     const newBlogs = blogs.filter(blog => blog.id !== id);
-    //     setBlogs(newBlogs);
-    // }
-
-    useEffect(() => { // runs for every render
-        console.log('use effect ran')
-        // setTimeout(() => { // Loading method to test
-            fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if(!res.ok) {
-                    throw Error('could not fetch the data for that ressource')
-                }
-                return res.json()
-            })
-            .then(data => {
-                // console.log(data)
-                setBlogs(data)
-                setIsPending(false)
-                setError(null)
-            })
-            .catch(err => { // catch network error
-                setIsPending(false)
-                setError(err.message)
-            })
-        // }, 1000)
-    }, []) // empty array, function only runs on the first initial render (once)
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
     return ( 
         <div className="home"> 
@@ -63,21 +18,6 @@ const Home = () => {
                     {/* handleDelete={handleDelete} */}
             {blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
             {/* konditional templating in react (blogs &&) */}
-
-            {/* <button onClick={() => setName('luigi')}>change name</button>
-            <p>{ name }</p> */}
-            
-            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs" handleDelete={handleDelete}/> */}
-
-            {/* <p>{ name } is { age} years old</p>
-            <button onClick={ handleClick2 }>Click me</button>
-
-            <br></br>
-            <br></br>
-            <br></br>
-
-            <button onClick={ handleClick }>Click me</button>
-            <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button> */}
         </div>
     );
 }
---
+++ src/useFetch.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react"
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(true) // loading the Data
+    const [error, setError] = useState(null)
+
+    useEffect(() => { // runs for every render
+        console.log('use effect ran')
+        setTimeout(() => { // Loading method to test
+            fetch(url)
+            .then(res => {
+                if(!res.ok) {
+                    throw Error('could not fetch the data for that ressource')
+                }
+                return res.json()
+            })
+            .then(data => {
+                // console.log(data)
+                setData(data)
+                setIsPending(false)
+                setError(null)
+            })
+            .catch(err => { // catch network error
+                setIsPending(false)
+                setError(err.message)
+            })
+        }, 1000)
+    }, [url]) // empty array, function only runs on the first initial render (once)
+
+    return { data, isPending, error }
+}
+
+export default useFetch
